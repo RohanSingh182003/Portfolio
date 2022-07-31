229b88034921e243819a67c416259a7c0909f180
Rohan Singh <rs7289579@gmail.com>
2022-07-31 11:29:19 +0530

    create about section

--- components/Navbar.js
+++ components/Navbar.js
@@ -43,22 +43,27 @@ const Navbar = () => {
                         variants={navItem}
                         whileTap="tap"
                         whileHover="hover"
-                        className='cursor-pointer'>Home</motion.li>
+                        className='cursor-pointer hover:underline-offset-4 hover:underline hover:text-blue-600'><a href="#home"> Home</a></motion.li>
                         <motion.li
                         variants={navItem}
                         whileTap="tap"
                         whileHover="hover"
-                        className='cursor-pointer'>About</motion.li>
+                        className='cursor-pointer hover:underline-offset-4 hover:underline hover:text-blue-600'><a href="#about"> About</a></motion.li>
                         <motion.li
                         variants={navItem}
                         whileTap="tap"
                         whileHover="hover"
-                        className='cursor-pointer'>Portfolio</motion.li>
+                        className='cursor-pointer hover:underline-offset-4 hover:underline hover:text-blue-600'><a href="#portfolio"> Portfolio</a></motion.li>
                         <motion.li
                         variants={navItem}
                         whileTap="tap"
                         whileHover="hover"
-                        className='cursor-pointer'>Contact</motion.li>
+                        className='cursor-pointer hover:underline-offset-4 hover:underline hover:text-blue-600'><a href="#projects"> Projects</a></motion.li>
+                        <motion.li
+                        variants={navItem}
+                        whileTap="tap"
+                        whileHover="hover"
+                        className='cursor-pointer hover:underline-offset-4 hover:underline hover:text-blue-600'><a href="#contact"> Contact</a></motion.li>
                     </ul>
                 </div>
                 <div className='md:hidden'>
@@ -77,10 +82,11 @@ const Navbar = () => {
                className='w-36 h-[100vh] bg-[#ffffffde] absolute top-0 right-0 px-4'>
                     <GrClose onClick={showSidebar} className='ml-auto mt-4 bg-white mr-2'/>
                     <ul className='space-y-2 mt-6'>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Portfolio</li>
-                        <li>Contact</li>
+                        <li><a href="#home"> Home</a></li>
+                        <li><a href="#about"> About</a></li>
+                        <li><a href="#portfolio"> Portfolio</a></li>
+                        <li><a href="#projects"> Projects</a></li>
+                        <li><a href="#contact"> Contact</a></li>
                     </ul>
                 </motion.div>}
             </motion.nav>
